2e6f24bc7908ff5f044f68883e79f45d828e4e63
“shedp” <sheenadiep@gmail.com>
2023-01-15 19:50:04 +0000

    amend db

--- src/db/init.js
+++ src/db/init.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 const pool = new Pool({
-    connectionString: process.env.DB_URL || 'postgres://hcwhtede:J7a8uihmawGG1D7xyzruhmOrbpjQ2C3f@mouse.db.elephantsql.com/hcwhtede'
+    connectionString: process.env.DB_URL || 'postgres://xcjbolhg:bzzwq9voNzITxRM6G9uoMi3ujyghsStU@snuffleupagus.db.elephantsql.com/xcjbolhg'
 });
 
 console.log("DB connection established")
--- src/db/seed.js
+++ src/db/seed.js
@@ -1,8 +1,8 @@
-const fs = require('fs')
+const fs = require('fs');
 require('dotenv').config();
 
-const db = require('./init')
+const db = require('./init');
 
-const seeds = fs.readFileSync('./db/seedGames.sql').toString()
+const seeds = fs.readFileSync('./db/seedGames.sql').toString();
 
 db.query(seeds).then(data => console.log("Set-up complete")).catch(error => console.log(error))
