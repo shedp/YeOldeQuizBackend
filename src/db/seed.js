b0776d789203126f18f47c580888bdcc8bf979ec
“shedp” <sheenadiep@gmail.com>
2023-01-15 18:02:49 +0000

    db folder

--- src/db/seed.js
+++ src/db/seed.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 
 const db = require('./init')
 
-const seeds = fs.readFileSync('./db/seedHabits.sql').toString()
+const seeds = fs.readFileSync('./db/seedGames.sql').toString()
 
 db.query(seeds).then(data => console.log("Set-up complete")).catch(error => console.log(error))
