182f7bf396b927fe3849e7ef4efb4d5e4e2aab7f
JLP2000 <josh15phan@gmail.com>
2023-01-15 22:53:45 +0000

    added controllers

--- src/routers/scores.js
+++ src/routers/scores.js
@@ -7,7 +7,7 @@ scoresRouter.get("/", scoresController.index) // get all scores from every user
 scoresRouter.get("/users/:id", scoresController.showByUser) // get all scores from a user
 scoresRouter.get("/game/:id", scoresController.showByGame) // get all scores from a user
 scoresRouter.post("/", scoresController.create) // creates scores for a game
-scoresRouter.put("/:id", scoresController.update) // update a score value
+scoresRouter.put("/:id", scoresController.update) // update a score value passing the round_if
 scoresRouter.delete("/:id", scoresController.destroy) // delete scores if a user decides not to create the game
 
 
