423deb6ac5df0e2d3fa68f2b3a3f3cdac9981ab0
JLP2000 <josh15phan@gmail.com>
2023-01-17 12:56:51 +0000

    changes to socket

--- src/server_socket.js
+++ src/server_socket.js
@@ -4,7 +4,7 @@ const app = express();
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
     cors: {
-        origin: ["http://localhost:8080"],
+        origin: '*',
         methods: ["GET", "POST"]
     }
 })
