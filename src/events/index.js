c3cde91e453ff05df66fecae412c1265ed40edf5
JLP2000 <josh15phan@gmail.com>
2023-01-17 17:29:21 +0000

    changes to socket events

--- src/events/index.js
+++ src/events/index.js
@@ -1,16 +1,18 @@
 const Game = require("../models/Game")
 
-const socketEvents = (socket) => {
+const socketEvents = async (socket) => {
     console.log("User Connected")
 
     socket.on("disconnect", () => console.log("User has disconnected"))
 
     socket.on("hello", () => console.log("hello"))
 
-    socket.on("create-game", ({join_code, level, topics}) => {
+    socket.on("create-game", async ({join_code, level, topics}) => {
         try{
             console.log(`Lobby created with join code: ${join_code}`)
             socket.join(join_code)
+            let clients = await socket.in(join_code).fetchSockets();
+            console.log(clients)
         } catch(err){
             console.log(err);
             socket.emit('error','couldnt perform create action');
@@ -20,7 +22,14 @@ const socketEvents = (socket) => {
     socket.on("join-game", ({join_code, username}) => {
         try{
             console.log(`${username} has joined the lobby (${join_code})`)
-            socket.join(join_code);            
+            socket.join(join_code); 
+            const users = Object.keys(socket.sockets.adapter.rooms[joinCode].sockets)
+            socket.to(joinCode).emit("updateUsers",users)
+            
+            socket.on("updateUsers", (joinCode)=> {
+                const users = object.keys(sockst.sockets.adapter.rooms[JoinCode].sockets)
+                socket.to(joinCode).emit("updateUsers",users)
+            })
         } catch(err){
             console.log(err);
             socket.emit('error','couldnt perform join action');
