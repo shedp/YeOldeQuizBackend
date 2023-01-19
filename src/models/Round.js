ce96eb2e0b601b06aca92df23ae2558e7632b7c1
adamminchella <adamminchella@msn.com>
2023-01-19 15:18:00 +0000

    Make post requests

--- src/models/Round.js
+++ src/models/Round.js
@@ -1,75 +1,73 @@
-const db = require("../db/init")
-const Score = require("./Score")
+const db = require("../db/init");
+const Score = require("./Score");
 
 class Round {
-	constructor(data) {
-		this.round_id = data.round_id
-		this.game_id = data.game_id
-		this.topic = data.topic
-	}
+  constructor(data) {
+    this.round_id = data.round_id;
+    this.game_id = data.game_id;
+    this.topic = data.topic;
+  }
 
-	//find by game_id
-	static findByGameId(id){
-		return new Promise(async (resolve, reject) => {
-			try {
-				let roundData = await db.query(
-					`SELECT * FROM rounds WHERE game_id = $1;`,
-					[id]
-				)
-				let target = roundData.rows.map(r => new Round(r))
-				resolve(target)
-			} catch (err) {
-				reject("Rounds not found")
-			}
-		})
-	}
+  //find by game_id
+  static findByGameId(id) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let roundData = await db.query(
+          `SELECT * FROM rounds WHERE game_id = $1;`,
+          [id]
+        );
+        let target = roundData.rows.map((r) => new Round(r));
+        resolve(target);
+      } catch (err) {
+        reject("Rounds not found");
+      }
+    });
+  }
 
-	// create rounds
-	static async create(user_id, id, topic) {
-		return new Promise(async (resolve, reject) => {
-			try {
-				const result = await db.query(
-					"INSERT INTO rounds (game_id, topic) VALUES ($1, $2) RETURNING *;",
-					[id, topic]
-				)
-				let newRound = new Round(result.rows[0])
-				await newRound.createScores(user_id)
-				resolve(newRound)
-			} catch (err) {
-				reject("Could not create round")
-				console.log(err)
-			}
-		})
-	}
+  // create rounds
+  static async create(user_id, id, topic) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const result = await db.query(
+          "INSERT INTO rounds (game_id, topic) VALUES ($1, $2) RETURNING *;",
+          [id, topic]
+        );
+        let newRound = new Round(result.rows[0]);
+        resolve(newRound);
+      } catch (err) {
+        reject("Could not create round");
+        console.log(err);
+      }
+    });
+  }
 
-	//create scores after creating rounds
-	async createScores(user_id) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                await Score.create(this.game_id, this.round_id, user_id)
-				
-                resolve("scores added")
-            } catch (err) {
-                reject("Could not add scores")
-                console.log(err)
-            }
-        })
-    }
+  //create scores after creating rounds
+  async createScores(user_id) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await Score.create(this.game_id, this.round_id, user_id);
 
-	destroy() {
-		return new Promise(async (resolve, reject) => {
-			try {
-				const result = await db.query(
-					"DELETE FROM rounds WHERE game_id IN ($1);",
-					[this.game_id]
-				)
-				resolve("round was destroyed")
-			} catch (err) {
-				reject("Could not destroy round")
-			}
-		})
-	}
-	
+        resolve("scores added");
+      } catch (err) {
+        reject("Could not add scores");
+        console.log(err);
+      }
+    });
+  }
+
+  destroy() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const result = await db.query(
+          "DELETE FROM rounds WHERE game_id IN ($1);",
+          [this.game_id]
+        );
+        resolve("round was destroyed");
+      } catch (err) {
+        reject("Could not destroy round");
+      }
+    });
+  }
 }
 
-module.exports = Round
+module.exports = Round;
--- src/routers/rounds.js
+++ src/routers/rounds.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const roundsRouter = express.Router()
-const roundsController = require("../controllers/rounds")
-const authenticator = require("../middleware/auth")
+const express = require("express");
+const roundsRouter = express.Router();
+const roundsController = require("../controllers/rounds");
+const authenticator = require("../middleware/auth");
 
-roundsRouter.post("/", roundsController.create) // create rounds for a game
-roundsRouter.delete("/:id", roundsController.destroy) // delete rounds when a user decides not to create a game
+roundsRouter.get("/:id", roundsController.getRoundIDs);
+roundsRouter.post("/", roundsController.create); // create rounds for a game
+roundsRouter.delete("/:id", roundsController.destroy); // delete rounds when a user decides not to create a game
 
 module.exports = roundsRouter;
